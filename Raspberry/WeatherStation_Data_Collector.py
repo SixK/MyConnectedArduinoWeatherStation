522b19c956f1bb38714e413684f6d465e2b0d379
SixK <dasixk@free.fr>
2015-11-15 13:37:50 +0100

    Arduino Source code added
    Arduino Source code added

--- Raspberry/WeatherStation_Data_Collector.py
+++ Raspberry/WeatherStation_Data_Collector.py
@@ -35,28 +35,21 @@ def getUri(uri) :
 
 
 def getDate() :
-	uri='http://cooptland.free.fr/meteo/getDate.php'
+	uri='http://yourserver.com/meteo/getDate.php'
 	myDate=getUri(uri)
 	print(myDate)
-	# droid.eventPost('display_data', myDate)  # send an event with the sensor data back to the html page.
-	# droid.eventClearBuffer()  # workaround for a bug in SL4A r4.
+
 	return myDate
 
 def syncDate() :
 	myDate = getDate()
-	# droid.bluetoothWrite('d '+myDate) 
 	btSerial.write('d '+myDate)
 
 btSerial = serial.Serial	("/dev/rfcomm1", baudrate=9600)
 syncDate()
 while True:
-	# result = droid.eventWaitFor('fetch_data').result
-	# droid.bluetoothWrite('m')  # get data from Arduino.
 	try :
 		btSerial.write('m')
-	
-		#data = droid.bluetoothReadLine().result  # read the line with the sensor value from arduino.
-		# droid.eventClearBuffer()  # workaround for a bug in SL4A r4.
 		data = btSerial.readline()
 	except e:
 		logging.error("Error with btSerial not handled !")
@@ -70,14 +63,12 @@ while True:
 	
 	if (data != ""):
 		processed_data = data.split(";")
-		# droid.eventPost('display_data', data)  # send an event with the sensor data back to the html page.
 		
-		uri="http://cooptland.free.fr/meteo/?add&"			
+		uri="http://yourserver.com/meteo/?add&"			
 		args = {'date':processed_data[0],'inttempc':processed_data[1],'inthumidity':processed_data[2],'pressure':processed_data[3],'exttempc':'-1','exthumidity':'-1'}
 		uri+=urllib.urlencode(args)
 
 		getUri(uri)
-
+		
+		# Wait 2 minutes
 		time.sleep(2 * 60)
-		# Wait 1 second between each measurement
-		# self.after(2*60000,self.measure)
